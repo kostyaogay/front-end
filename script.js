9b6383159f5f851c5b11fa47caddca1cdbd79a8e
Kostya Ogay~ <olekawowi@gmail.com>
2023-09-08 19:33:28 +0500

    first commit

--- script.js
+++ script.js
@@ -24,8 +24,29 @@ myButton.addEventListener('click', function() {
 	addText.innerHTML += 'Я добавил текст по клику! '
 })
 
-
-
-
-
-
+//событие мыши
+const firstScreenImage = document.querySelector('.first-screen__image')
+console.log(firstScreenImage)
+// firstScreenImage.addEventListener('mousemove', function() {
+// 	console.log('движение мыши')
+// })
+firstScreenImage.addEventListener('mouseenter', function() {
+	console.log('Навел курсор на картинку')
+  firstScreenImage.classList.add('active') 
+  console.log(firstScreenImage)
+})
+firstScreenImage.addEventListener('mouseleave', function() {
+	console.log('Убрал курсор с картинки')
+  firstScreenImage.classList.remove('active') 
+  console.log(firstScreenImage)
+})
+ //Шаг №1 находим нужный элемент по селектору в HTML используя JS и метод querySelector()
+ const footer = document.querySelector('.footer')
+ //Шаг №2 подключаем слушатель события с методом click на элемент(селектора footer)
+ // при помощи addEventListener()
+ footer.addEventListener('mouseenter' , function() {
+  console.log('Навели курсор на footer')
+  
+ })
+ footer.addEventListener('mouseleave' , function() {
+  console.log('Убрали курсор с footer')})
